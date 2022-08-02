3e6ed2a8b87dbdb040fa5c1ab9470c5583c91e5b
santo-arafath <66453960+santo-arafath@users.noreply.github.com>
2022-08-02 22:00:35 +0600

    Basic array

---
+++ BasicArray.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+  int arr[100]={2};
+
+  int n,i=1;
+  cin>>n;
+  while(++i<n)
+  {
+
+    cin>>arr[i];
+
+
+  }
+  int j=-1;
+     while(++j<100)
+  {
+
+    cout<<arr[j]<<" ";
+
+
+  }
+
+    return 0;
+}
